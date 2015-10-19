96fef8f8c4b8e43a16ceef9f4c28f71ccf0e1f59
Sallar Kaboli <sallar.kaboli@gmail.com>
2015-10-19 21:30:19 +0300

    Cleaned up app.js

--- source/app/app.js
+++ source/app/app.js
@@ -1,41 +1,25 @@
-var app = require('app'), // Module to control application life.
-    path = require('path');
-var BrowserWindow = require('browser-window');  // Module to create native browser window.
+"use strict";
 
-// Report crashes to our server.
-require('crash-reporter').start();
+var app = require('app'),
+    path = require('path'),
+    BrowserWindow = require('browser-window');
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
+// Global reference of the window object
 var mainWindow = null;
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function() {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  //if (process.platform != 'darwin') {
     app.quit();
-  //}
 });
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
 app.on('ready', function() {
-  // Create the browser window.
-  mainWindow = new BrowserWindow({width: 400, height: 600});
-  console.log("READY");
+    // Create the browser window.
+    mainWindow = new BrowserWindow({width: 800, height: 600});
+    mainWindow.loadUrl('file://' + path.resolve(__dirname, '../public/index.html'));
+    mainWindow.openDevTools();
 
-  // and load the index.html of the app.
-  mainWindow.loadUrl('file://' + path.resolve(__dirname, '../public/index.html'));
-
-  // Open the DevTools.
-  //mainWindow.openDevTools();
-
-  // Emitted when the window is closed.
-  mainWindow.on('closed', function() {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null;
-  });
+    // Emitted when the window is closed.
+    mainWindow.on('closed', function() {
+        mainWindow = null;
+    });
 });
