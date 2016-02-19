678125faa3b2b8cec336d177d1d23b2d8d7ec82a
Naitian Zhou <nzhou2011@gmail.com>
2016-02-18 22:35:34 -0500

    small fix

--- source/resources/js/app/views/entry.js
+++ source/resources/js/app/views/entry.js
@@ -178,7 +178,7 @@ export default Backbone.View.extend({
     removeEntry: function () {
         confirmDialog(
             `Delete ${this.model.get("title")}?`,
-            `Are you sure you want to delete this entry? This cannot be undone.`,
+            `Are you sure you want to delete this entry?`,
             "Delete",
             (confirm) => {
                 if (confirm === true) {
