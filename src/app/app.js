3c915f58cc06f31090030fc04244ee223ac32fbf
Sallar Kaboli <sallar.kaboli@gmail.com>
2016-07-03 20:58:22 +0300

    Clean up app.js

--- src/app/app.js
+++ src/app/app.js
@@ -1,81 +1,80 @@
-"use strict";
+import path from 'path';
+import { app, BrowserWindow, Menu } from 'electron'; // eslint-disable-line import/no-extraneous-dependencies
+import WindowManager from './WindowManager';
+import eventsManager from './events';
+import menuTemplate from './config/menu';
+import Platform from './lib/platform';
 
-var electron        = require('electron'),
-    app             = electron.app,
-    BrowserWindow   = electron.BrowserWindow,
-    Menu            = electron.Menu,
-    ipc             = electron.ipcMain,
-    path            = require('path'),
-    EventsManager   = require('./events.js')(),
-    windowManager   = require('./WindowManager.js').getSharedInstance(),
-    menuTemplate    = require('./config/menu.js'),
-    Platform        = require("./lib/platform");
+const windowManager = WindowManager.getSharedInstance();
+eventsManager();
 
 // Global reference of the window object
 global.Buttercup = {
-    config: {
-        publicDir: 'file://' + path.resolve(__dirname, '../../dist')
-    }
+  config: {
+    publicDir: `file://${path.resolve(__dirname, '../../dist')}`
+  }
 };
 
 // Intro Screen
-windowManager.setBuildProcedure("intro", function openIntroScreen() {
-    // Create the browser window.
-    var introScreen = new BrowserWindow({
-        width: 700,
-        height: 500,
-        'title-bar-style': 'hidden'
-    });
-    introScreen.loadURL(Buttercup.config.publicDir + '/index.html');
-    //introScreen.webContents.openDevTools();
+windowManager.setBuildProcedure('intro', () => {
+  // Create the browser window.
+  const introScreen = new BrowserWindow({
+    'width': 700,
+    'height': 500,
+    'title-bar-style': 'hidden'
+  });
+  introScreen.loadURL(`${global.Buttercup.config.publicDir}/index.html`);
+  // introScreen.webContents.openDevTools();
 
-    // Emitted when the window is closed.
-    introScreen.on('closed', function() {
-        // Deregister the intro screen
-        windowManager.deregister(introScreen);
-        if (windowManager.getCountOfType("archive") <= 0) {
-            // No archives open, exit app
-            app.quit();
-        }
-    });
+  // Emitted when the window is closed.
+  introScreen.on('closed', () => {
+    // Deregister the intro screen
+    windowManager.deregister(introScreen);
+    if (windowManager.getCountOfType('archive') <= 0) {
+      // No archives open, exit app
+      app.quit();
+    }
+  });
 
-    return introScreen;
+  return introScreen;
 });
 
 const installExtensions = async () => {
   if (process.env.NODE_ENV === 'development') {
-    const installer = require('electron-devtools-installer'); // eslint-disable-line global-require
+    const installer = require('electron-devtools-installer'); // eslint-disable global-require
+
+    const forceDownload = Boolean(process.env.UPGRADE_EXTENSIONS);
     const extensions = [
       'REACT_DEVELOPER_TOOLS',
       'REDUX_DEVTOOLS'
     ];
-    const forceDownload = !!process.env.UPGRADE_EXTENSIONS;
+
     for (const name of extensions) {
       try {
-        await installer.default(installer[name], forceDownload);
-      } catch (e) {} // eslint-disable-line
+        await installer.default(installer[name], forceDownload); // eslint-disable-line babel/no-await-in-loop
+      } catch (e) {} // eslint-disable-line xo/catch-error-name
     }
   }
 };
 
 // When user closes all windows
-app.on('window-all-closed', function() {
-    // Reopen the Intro window
-    if (windowManager.getCountOfType("archive") <= 0) {
-        windowManager.buildWindowOfType("intro");
-    }
+app.on('window-all-closed', () => {
+  // Reopen the Intro window
+  if (windowManager.getCountOfType('archive') <= 0) {
+    windowManager.buildWindowOfType('intro');
+  }
 });
 
-app.on('ready', async function() {
+app.on('ready', async () => {
   await installExtensions();
 
-    // Show intro
-    windowManager.buildWindowOfType("intro");
+  // Show intro
+  windowManager.buildWindowOfType('intro');
 
-    // Show standard menu
-    if (Platform.isOSX()) {
-        Menu.setApplicationMenu(
-            Menu.buildFromTemplate(menuTemplate)
-        );
-    }
+  // Show standard menu
+  if (Platform.isOSX()) {
+    Menu.setApplicationMenu(
+      Menu.buildFromTemplate(menuTemplate)
+    );
+  }
 });
