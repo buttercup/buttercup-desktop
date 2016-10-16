d3c24988f59e8374661279ba16f30e1192935085
Salar Kaboli <sallar.kaboli@gmail.com>
2016-10-16 12:12:33 +0300

    Remove react router

--- src/renderer/index.js
+++ src/renderer/index.js
@@ -2,18 +2,14 @@ import 'buttercup-web';
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-import { Router, hashHistory } from 'react-router';
-import routes from './routes';
 import configureStore from './redux/configureStore';
+import WorkspaceContainer from './containers/Workspace';
 
-const store = configureStore({
-  recentFiles: [],
-  groups: []
-});
+const store = configureStore();
 
 render(
   <Provider store={store}>
-    <Router routes={routes} history={hashHistory}/>
+    <WorkspaceContainer/>
   </Provider>,
   document.getElementById('root')
 );
