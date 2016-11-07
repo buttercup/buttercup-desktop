65b047344d986985f07f8bb8c89016699e60960b
Sallar Kaboli <sallar.kaboli@gmail.com>
2016-11-07 22:32:34 +0200

    fix(dist): remove extra test files

--- tests/setup.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const path = require('path');
-
-const sourcePath = path.join(__dirname, '../source');
-
-module.exports = {
-  WindowManager: require(path.join(sourcePath, '/app/WindowManager.js'))
-};
