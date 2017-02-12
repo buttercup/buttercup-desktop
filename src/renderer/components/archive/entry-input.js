417d9b4889e05db10da1922c8e8c84ae2cee7d9a
Sallar Kaboli <sallar.kaboli@gmail.com>
2017-02-12 13:42:38 +0200

    Upgrade Meter to use buttercup-ui

--- src/renderer/components/archive/entry-input.js
+++ src/renderer/components/archive/entry-input.js
@@ -1,8 +1,7 @@
 import React, { Component, PropTypes } from 'react';
 import cx from 'classnames';
-import { Generator, Button } from 'buttercup-ui';
+import { Generator, Meter } from 'buttercup-ui';
 import MagicIcon from 'react-icons/lib/fa/magic';
-import Meter from '../meter';
 import styles from '../../styles/entry-input';
 
 const BareInput = ({input, name, placeholder, type}) => {
