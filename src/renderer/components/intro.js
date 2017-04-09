2a8cf6f68c031c19d7604dac5b5337cf98bb0f92
Sallar Kaboli <sallar.kaboli@gmail.com>
2017-04-09 11:39:53 +0300

    Improve File Opener styling

--- src/renderer/components/intro.js
+++ src/renderer/components/intro.js
@@ -1,13 +1,13 @@
 import React from 'react';
+import { Flex } from 'styled-flexbox';
 import RecentFiles from '../containers/recent-files';
 import FileOpener from '../containers/file-opener';
-import { wrapper } from '../styles/intro';
 
 const Intro = () => (
-  <div className={wrapper}>
+  <Flex flexAuto>
     <FileOpener/>
     <RecentFiles/>
-  </div>
+  </Flex>
 );
 
 export default Intro;
