d0ef7abfd15e2f20f38f04f85fdf477d648561b5
Sallar Kaboli <sallar.kaboli@gmail.com>
2017-10-01 18:48:44 +0300

    More secure tracking

--- src/main/actions.js
+++ src/main/actions.js
@@ -1,9 +1,18 @@
-import { ipcMain as ipc, BrowserWindow } from 'electron';
+import { ipcMain as ipc, BrowserWindow, app } from 'electron';
+import PiwikTracker from 'piwik-tracker';
+import log from 'electron-log';
 import { pushUpdate, updateInstalled } from '../shared/actions/update';
 import { getWindowManager } from './lib/window-manager';
 import { startAutoUpdate, installUpdates } from './lib/updater';
 import { openFile, newFile, openFileForImporting } from './lib/files';
 import { setupMenu } from './menu';
+
+// Track Launches (anonymous)
+const piwik = new PiwikTracker(2, 'https://analytics.buttercup.pw/piwik.php');
+
+piwik.on('error', function(err) {
+  log.error('Tracking error', err);
+});
 
 const windowManager = getWindowManager();
 
@@ -45,5 +54,17 @@ export function setupActions(store) {
   ipc.on('show-import-dialog', (e, payload) => {
     const { type, archiveId } = payload;
     openFileForImporting(undefined, type, archiveId);
+  });
+
+  ipc.on('track', (e, payload) => {
+    piwik.track({
+      url: 'buttercup://desktop/beta',
+      action_name: 'Launch',
+      ua: payload,
+      cvar: JSON.stringify({
+        '1': ['version', app.getVersion()],
+        '2': ['production', process.env.NODE_ENV === 'development' ? 'No' : 'Yes']
+      })
+    });
   });
 }
