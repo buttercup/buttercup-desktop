2f4250e584456b478aac0aa023febd96cc1ce506
ph1p <phil@capshake.com>
2017-11-16 22:59:34 +0100

    Check if the focused text field remains in focus Add test Add ava config

--- test/app.js
+++ test/app.js
@@ -1,24 +1,35 @@
 import path from 'path';
 import test from 'ava';
+import { ArchiveTypes } from '../src/shared/buttercup/types';
+import { loadFile } from '../src/main/lib/files';
 import { Application } from 'spectron';
 
-test.beforeEach(async t => {
-  t.context.app = new Application({
+const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
+
+let app;
+let buttons;
+let inputs;
+test.before(async t => {
+  app = new Application({
     path: require('electron'),
     args: [path.join(__dirname, '../app')]
   });
 
-  await t.context.app.start();
+  await app.start();
+  await app.client.waitUntilWindowLoaded();
 });
 
-test.afterEach.always(async t => {
-  await t.context.app.stop();
+test.after(async t => {
+  await app.stop();
 });
+
+const payload = {
+  type: ArchiveTypes.FILE,
+  path: path.resolve(__dirname, './test.bcup'),
+  isNew: true
+};
 
 test(async t => {
-  const app = t.context.app;
-  await app.client.waitUntilWindowLoaded();
-
   const win = app.browserWindow;
   t.is(await app.client.getWindowCount(), 1);
   t.false(await win.isMinimized());
@@ -29,4 +40,41 @@ test(async t => {
   const { width, height } = await win.getBounds();
   t.true(width > 0);
   t.true(height > 0);
+});
+
+test('test input focus', async t => {
+  // create temp archive
+  await app.webContents.send('load-archive', payload);
+
+  await sleep(500);
+  await app.client.setValue('input[type="password"]', '1');
+  await app.client.keys('Enter');
+
+  await app.client.setValue('input[type="password"]', '1');
+  await app.client.keys('Enter');
+
+  await sleep(2000);
+
+  // click add entry
+  buttons = await app.client.elements('button');
+  await app.client.elementIdClick(buttons.value[3].ELEMENT);
+
+  await sleep(1000);
+
+  // entry is open
+  buttons = await app.client.elements('button');
+  inputs = await app.client.elements('input');
+  await app.client.elementIdClick(buttons.value[4].ELEMENT);
+
+  // set title
+  await app.client.setValue('input[name="properties.title"]', 'title');
+
+  // get title
+  const titleInput = await app.client.getValue(
+    'input[name="properties.title"]'
+  );
+  console.log('inputs', titleInput);
+
+  // test title
+  t.true(titleInput === 'title');
 });
