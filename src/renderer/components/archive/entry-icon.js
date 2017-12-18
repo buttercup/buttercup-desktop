dad03e5b768a6e952e5b93bcb7de18d209bc8af2
Sallar Kaboli <sallar.kaboli@gmail.com>
2017-12-18 20:46:12 +0200

    Fix typo in entry-icon component

--- src/renderer/components/archive/entry-icon.js
+++ src/renderer/components/archive/entry-icon.js
@@ -3,15 +3,15 @@ import styled from 'styled-components';
 import defaultIcon from '../../styles/img/ic-lock-48px.svg';
 
 const EntryIcon = styled.div`
-  height: ${props => (props.big ? '50px' : '30px')}
-  width: ${props => (props.big ? '50px' : '30px')}
-  
+  height: ${props => (props.big ? '50px' : '30px')};
+  width: ${props => (props.big ? '50px' : '30px')};
+
   border-radius: 5%;
   background-color: white;
   border: 3px solid white;
-  
-  background-image: ${props => `url(${props.icon ? props.icon : defaultIcon})`}
-  background-size: 99%;   
+
+  background-image: ${props => `url(${props.icon ? props.icon : defaultIcon})`};
+  background-size: 99%;
   background-repeat: no-repeat;
   background-position: center;
 `;
