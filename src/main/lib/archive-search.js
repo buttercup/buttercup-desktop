a7818bcc02248bf1c43d5121c581ffd59196a1e2
ph1p <phil@capshake.com>
2018-02-23 14:40:40 +0100

    Remove unused vars

--- src/main/lib/archive-search.js
+++ src/main/lib/archive-search.js
@@ -1,10 +1,3 @@
-import path from 'path';
-import { ipcMain as ipc } from 'electron';
-import i18n from '../../shared/i18n';
-import { getWindowManager } from './window-manager';
-
-const windowManager = getWindowManager();
-
 /**
  * Present an open dialog box
  * and return the filename if selected
