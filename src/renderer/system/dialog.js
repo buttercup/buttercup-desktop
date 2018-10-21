73dd8a9dcc91c4ae13bf5ddb7013bbe35f7cab01
Antoine AMARA <amara.antoine@gmail.com>
2018-10-21 17:21:10 +0200

    feat: check master password strength when creating a new archive.
    to be more secure, check the master password strength when creating an archive
    and return an error if the password doesn't follow the security
    recommendations.
    
    for that, follow the main recommendations from the OWASP association.
    the master password should now contain:
      - one uppercase letter.
      - one lowercase letter.
      - one number.
      - one special character.
    
    Fixes: https://github.com/buttercup/buttercup-desktop/issues/62
    Refs: https://www.owasp.org/index.php/Authentication_Cheat_Sheet#Implement_Proper_Password_Strength_Controls

--- src/renderer/system/dialog.js
+++ src/renderer/system/dialog.js
@@ -24,8 +24,30 @@ export function showDialog(message, type = 'error') {
   });
 }
 
+/**
+ * _checkPasswordStrength
+ * Check password security rules
+ * Password should have:
+ *   - one uppercase letter minimum.
+ *   - one lowercase letter minimum.
+ *   - one number.
+ *   - one special character.
+ *   - it should be > 10 < 128 characters.
+ * @param {string} password password to check.
+ * @return true if the paswoard is secure, false otherwise.
+ */
+function _checkPasswordStrength(password) {
+  const strongPasswordRegex =
+    '^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{10,})';
+
+  return new RegExp(strongPasswordRegex).test(password);
+}
+
 export function showPasswordDialog(preConfirm, options = {}) {
-  const defaultFunc = password => Promise.resolve(password);
+  const defaultFunc = password =>
+    _checkPasswordStrength(password)
+      ? Promise.resolve(password)
+      : Promise.reject(i18n.t('password-dialog.not-strong-enought'));
   if (typeof preConfirm === 'object') {
     options = preConfirm;
     preConfirm = defaultFunc;
