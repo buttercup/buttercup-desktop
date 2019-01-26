d88f408bb2ef701d9589ea25207822f4ec738b3b
ph1p <phil@capshake.com>
2019-01-26 17:55:40 +0100

    Fix shortcut bug with tray icon enabled
    Set buttercup to 2.7.0 to prevent errors

--- src/main/global-shortcuts.js
+++ src/main/global-shortcuts.js
@@ -1,28 +1,36 @@
 import { globalShortcut, ipcMain as ipc } from 'electron';
 import { getWindowManager } from './lib/window-manager';
-import { sendEventToMainWindow } from './utils/window';
+import { sendEventToMainWindow, reopenMainWindow } from './utils/window';
 import { setupMenu } from './menu';
 import { getSetting } from '../shared/selectors';
 import { DEFAULT_GLOBAL_SHORTCUTS } from '../shared/utils/global-shortcuts';
 
 const windowManager = getWindowManager();
-const windows = windowManager._windows;
 
 // unregister all global shortcuts
 export const unregisterGlobalShortcuts = () => globalShortcut.unregisterAll();
 
 // defined global shortcut methods
 const initGlobalShortcuts = {
-  'preferences.minimize-and-maximize': shortcut =>
+  'preferences.minimize-and-maximize': (shortcut, store) =>
     globalShortcut.register(shortcut, () => {
-      if (windows) {
-        const [window] = windowManager.getWindowsOfType('main');
+      const state = store.getState();
+      const [window] = windowManager.getWindowsOfType('main');
 
-        if (window.isMinimized()) {
-          window.focus();
-          window.restore();
+      if (getSetting(state, 'isTrayIconEnabled')) {
+        if (!window) {
+          reopenMainWindow();
         } else {
-          window.minimize();
+          window.close();
+        }
+      } else {
+        if (window) {
+          if (window.isMinimized()) {
+            window.focus();
+            window.restore();
+          } else {
+            window.minimize();
+          }
         }
       }
     })
@@ -61,7 +69,7 @@ export const setupGlobalShortcuts = store => {
   Object.keys(globalShortcuts).forEach(
     shortcutName =>
       initGlobalShortcuts[shortcutName] &&
-      initGlobalShortcuts[shortcutName](globalShortcuts[shortcutName])
+      initGlobalShortcuts[shortcutName](globalShortcuts[shortcutName], store)
   );
 
   ipc.on('register-global-shortcut', (e, { name, accelerator }) => {
@@ -80,7 +88,7 @@ export const setupGlobalShortcuts = store => {
 
         // register new defined shortcut
         if (initGlobalShortcuts[name]) {
-          initGlobalShortcuts[name](accelerator);
+          initGlobalShortcuts[name](accelerator, store);
         }
 
         // refresh menu
