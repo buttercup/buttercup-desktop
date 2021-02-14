d03cb1067449c91c5233124c4dae12db69eb5a0c
Perry Mitchell <perry@perrymitchell.net>
2021-02-14 21:46:01 +0200

    Remove hard-coded dropbox token

--- source/renderer/services/auth3rdParty.ts
+++ source/renderer/services/auth3rdParty.ts
@@ -3,7 +3,6 @@ import { remote } from "electron";
 const { BrowserWindow } = remote;
 
 export async function authenticate(authURL: string, matchRegex: RegExp): Promise<string | null> {
-    return "5wuMUtR1AewAAAAAAAAAAdBee4QWq5WQIN8LjfzZx0i3nhgFUoSe_b4nn-y7mMAa"; // @todo remove
     const currentWindow = BrowserWindow.getFocusedWindow();
     return new Promise<string>(resolve => {
         let foundToken = null;
@@ -36,9 +35,6 @@ export async function authenticate(authURL: string, matchRegex: RegExp): Promise
         };
 
         authWin.webContents.on("did-start-navigation", (e, url) => navigateCB(url));
-        // authWin.webContents.on("did-get-redirect-request", (e, oldUrl, newUrl) =>
-        //     navigateCB(newUrl)
-        // );
         authWin.webContents.on("will-redirect", (e, url) => navigateCB(url));
         authWin.on("hide", closeCB);
         authWin.on("close", closeCB);
