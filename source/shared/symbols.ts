49d415624729b1e059b9e54d88e5730d9523f70f
Perry Mitchell <perry@perrymitchell.net>
2021-02-19 19:25:12 +0200

    Add google auth procedure

--- source/shared/symbols.ts
+++ source/shared/symbols.ts
@@ -1,2 +1,5 @@
 export const DROPBOX_CLIENT_ID = "5fstmwjaisrt06t";
+export const GOOGLE_AUTH_REDIRECT = "https://buttercup.pw?googledesktopauth";
+export const GOOGLE_CLIENT_ID = "327941947801-fumr4be9juk0bu3ekfuq9fr5bm7trh30.apps.googleusercontent.com";
+export const GOOGLE_CLIENT_SECRET = "2zCBNDSXp1yIu5dyE5BVUWQZ";
 export const ICON_UPLOAD = "upload";
