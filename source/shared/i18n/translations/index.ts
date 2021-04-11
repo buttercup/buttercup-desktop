88d97a8f2574bb825f3e35a3c1c6e0fc907ea128
Julian Pömp <j.poemp@campus.lmu.de>
2021-04-12 00:19:35 +0200

    added translation to German

--- source/shared/i18n/translations/index.ts
+++ source/shared/i18n/translations/index.ts
@@ -1,10 +1,12 @@
 import en from "./en.json";
+import de from "./de.json";
 import fr from "./fr.json";
 import pl from "./pl.json";
 
 export default {
     en, // Keep as first item
     // All others sorted alphabetically:
+    de,
     fr,
     pl
 };
