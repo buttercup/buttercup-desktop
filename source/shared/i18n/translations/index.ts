59760d8011762deae49f125b9731a3e6d53a8dbc
Situ <lmarfany@gmail.com>
2021-05-05 09:27:13 +0200

    i18n: Added Spanish translation

--- source/shared/i18n/translations/index.ts
+++ source/shared/i18n/translations/index.ts
@@ -1,11 +1,13 @@
 import en from "./en.json";
 import de from "./de.json";
+import es from "./es.json";
 import fr from "./fr.json";
 import pl from "./pl.json";
 
 export default {
     en, // Keep as first item
     // All others sorted alphabetically:
+    es,
     de,
     fr,
     pl
