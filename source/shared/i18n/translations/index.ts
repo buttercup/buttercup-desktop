01e0b74ae8c98b04f9a3e2e9ae011f6b394ef151
Frédéric Momméja <fmommeja@free.fr>
2021-05-17 21:15:26 +0200

    Update index.ts

--- source/shared/i18n/translations/index.ts
+++ source/shared/i18n/translations/index.ts
@@ -1,12 +1,16 @@
 import en from "./en.json";
+import ca_es from "./ca_es.json";
+import de from "./de.json";
+import es from "./es.json";
 import fr from "./fr.json";
 import pl from "./pl.json";
-import se from "./se.json";
 
 export default {
     en, // Keep as first item
     // All others sorted alphabetically:
+    ca_es,
+    de,
+    es,
     fr,
-    pl,
-    se
+    pl
 };
