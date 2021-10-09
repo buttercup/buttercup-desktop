29caf6e001469678fab82a774d984afba492e988
MarkusJx <28785953+markusjx@users.noreply.github.com>
2021-10-09 23:01:43 +0200

    Fix asar load bug
    Signed-off-by: Markus <28785953+MarkusJx@users.noreply.github.com>

--- source/main/services/biometrics/WindowsBiometricProvider.ts
+++ source/main/services/biometrics/WindowsBiometricProvider.ts
@@ -1,8 +1,14 @@
 import BiometricProvider from "./BiometricProvider";
 import { VaultSourceID } from "buttercup";
-import { CredentialStore, Passport, nodeMsPassportAvailable } from "node-ms-passport";
+import {
+    CredentialStore,
+    Passport,
+    PassportModule,
+    PassportError,
+    PassportErrorCode
+} from "node-ms-passport";
 import { APP_ID } from "../../../shared/symbols";
-import { logWarn } from "../../library/log";
+import { logInfo, logWarn } from "../../library/log";
 import { Layerr } from "layerr";
 import crypto from "crypto";
 import { updateAppMenu } from "../../actions/appMenu";
@@ -16,10 +22,9 @@ import { updateAppMenu } from "../../actions/appMenu";
  * key and iv to be used with an aes encryption instance.
  * The aes instance is then used to encrypt the
  * password. After that, the password is stored in
- * the windows credential vault and protected using
- * CredProtectW. The random bytes are stored as the
- * user name encoded as a base64 string. The account
- * id consists of the app id and the vault id.
+ * the windows credential vault. The random bytes are
+ * stored as the user name encoded as a base64 string.
+ * The account id consists of the app id and the vault id.
  *
  * The key retrieval process is just the store process
  * in reverse except that the random bytes stored in
@@ -36,31 +41,31 @@ export default class WindowsBiometricProvider implements BiometricProvider {
      * The type of cipher used to encrypt the password
      * @private
      */
-    private static readonly CIPHER: string = 'aes-256-cbc';
+    private static readonly CIPHER: string = "aes-256-cbc";
 
     /**
      * The encoding for any buffers which contain raw bytes
      * @private
      */
-    private static readonly BYTE_ENCODING: BufferEncoding = 'base64';
+    private static readonly BYTE_ENCODING: BufferEncoding = "base64";
 
     /**
      * The encoding to encode and decode strings
      * @private
      */
-    private static readonly STRING_ENCODING: BufferEncoding = 'utf16le';
+    private static readonly STRING_ENCODING: BufferEncoding = "utf16le";
 
     /**
      * The hash algorithm used to create the aes key
      * @private
      */
-    private static readonly KEY_HASH_TYPE: string = 'sha256';
+    private static readonly KEY_HASH_TYPE: string = "sha256";
 
     /**
      * The hash algorithm used to create the aes initialization vector
      * @private
      */
-    private static readonly IV_HASH_TYPE: string = 'md5';
+    private static readonly IV_HASH_TYPE: string = "md5";
 
     /**
      * The number of random bytes to generate to sign
@@ -68,6 +73,24 @@ export default class WindowsBiometricProvider implements BiometricProvider {
      * @private
      */
     private static readonly CHALLENGE_LENGTH: number = 128;
+
+    /**
+     * Create a windows biometric provider instance.
+     * Will check if the module is packaged in an
+     * .asar file and if it is, replaces the 'app.asar'
+     * part with 'app.asar.unpacked' to fix a bug which
+     * causes electron to not load unpacked modules.
+     */
+    public constructor() {
+        if (!PassportModule.available()) return;
+        if (PassportModule.csModuleLocation.includes("\\app.asar\\")) {
+            // This is packaged in an .asar file
+            PassportModule.csModuleLocation = PassportModule.csModuleLocation.replace(
+                "\\app.asar\\",
+                "\\app.asar.unpacked\\"
+            );
+        }
+    }
 
     /**
      * Disable the biometric unlock for a vault.
@@ -80,16 +103,29 @@ export default class WindowsBiometricProvider implements BiometricProvider {
      * @param sourceID the vault id
      */
     public async disableSourceBiometricUnlock(sourceID: VaultSourceID): Promise<void> {
-        // Remove the account
-        const store = new CredentialStore(`${APP_ID}/${sourceID}`, true);
-        await store.remove();
+        try {
+            // Remove the account
+            const store = new CredentialStore(`${APP_ID}/${sourceID}`, false);
+            await store.remove();
 
-        const accounts = await CredentialStore.enumerateAccounts(`${APP_ID}/*`);
-        if (accounts.length === 0) {
-            // We do not own any more accounts,
-            // remove the passport account
-            const hello = new Passport(APP_ID);
-            await hello.deletePassportAccount();
+            const accounts = await CredentialStore.enumerateAccounts(`${APP_ID}/*`);
+            if (accounts.length === 0) {
+                // We do not own any more accounts,
+                // remove the passport account
+                const hello = new Passport(APP_ID);
+                await hello.deletePassportAccount();
+            }
+        } catch (err) {
+            logWarn("Could not delete the password from the credential vault", err);
+            throw new Layerr(
+                {
+                    cause: err,
+                    info: {
+                        i18n: "notification.error.biometrics-disable-failed"
+                    }
+                },
+                `Failed to delete the password from the credential vault for source: ${sourceID}`
+            );
         }
 
         await updateAppMenu();
@@ -110,7 +146,7 @@ export default class WindowsBiometricProvider implements BiometricProvider {
     public async getSourcePasswordViaBiometrics(sourceID: VaultSourceID): Promise<string> {
         try {
             // Read the data from the credential store
-            const store = new CredentialStore(`${APP_ID}/${sourceID}`, true);
+            const store = new CredentialStore(`${APP_ID}/${sourceID}`, false);
             const cred = await store.read();
 
             // Get the challenge from the username and get the key and iv
@@ -134,6 +170,14 @@ export default class WindowsBiometricProvider implements BiometricProvider {
             // Return the password as a string
             return password.toString(WindowsBiometricProvider.STRING_ENCODING);
         } catch (err) {
+            if (
+                err instanceof PassportError &&
+                err.getCode() == PassportErrorCode.ERR_USER_CANCELLED
+            ) {
+                logInfo(`Windows hello unlock cancelled by user for source: ${sourceID}`);
+                return null;
+            }
+
             logWarn("Windows hello failed", err);
             throw new Layerr(
                 {
@@ -199,7 +243,7 @@ export default class WindowsBiometricProvider implements BiometricProvider {
             const encrypted: string = data.toString(WindowsBiometricProvider.BYTE_ENCODING);
 
             // Store the challenge and password
-            const credentialStore = new CredentialStore(`${APP_ID}/${sourceID}`, true);
+            const credentialStore = new CredentialStore(`${APP_ID}/${sourceID}`, false);
             await credentialStore.write(
                 challenge.toString(WindowsBiometricProvider.BYTE_ENCODING),
                 encrypted
@@ -233,7 +277,7 @@ export default class WindowsBiometricProvider implements BiometricProvider {
      * @returns true if this system supports biometric unlock
      */
     public async supportsBiometricUnlock(): Promise<boolean> {
-        return nodeMsPassportAvailable() && Passport.passportAvailable();
+        return PassportModule.available() && Passport.passportAvailable();
     }
 
     /**
@@ -256,10 +300,12 @@ export default class WindowsBiometricProvider implements BiometricProvider {
 
         const signature: Buffer = await passport.passportSign(challenge);
 
-        const key = crypto.createHash(WindowsBiometricProvider.KEY_HASH_TYPE)
+        const key = crypto
+            .createHash(WindowsBiometricProvider.KEY_HASH_TYPE)
             .update(signature)
             .digest();
-        const iv = crypto.createHash(WindowsBiometricProvider.IV_HASH_TYPE)
+        const iv = crypto
+            .createHash(WindowsBiometricProvider.IV_HASH_TYPE)
             .update(signature)
             .digest();
 
@@ -277,7 +323,7 @@ export default class WindowsBiometricProvider implements BiometricProvider {
      * @private
      */
     private static zeroBuffers(...buf: Buffer[]): void {
-        buf.forEach(b => b.fill(0));
+        buf.forEach((b) => b.fill(0));
     }
 }
 
