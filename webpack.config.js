336d5197eced9ba069bc8827ede51d42f7c01a48
Perry Mitchell <perry@perrymitchell.net>
2022-06-18 20:00:23 +0300

    Fix styles

--- webpack.config.js
+++ webpack.config.js
@@ -22,72 +22,81 @@ module.exports = [
                         {
                             loader: "ts-loader",
                             options: {
-                                configFile: path.resolve(__dirname, "./tsconfig.web.json"),
-                            },
-                        },
+                                configFile: path.resolve(__dirname, "./tsconfig.web.json")
+                            }
+                        }
                     ],
-                    exclude: /node_modules/,
+                    exclude: /node_modules/
                 },
                 {
                     test: /\.pug$/,
-                    use: "pug-loader",
+                    use: "pug-loader"
                 },
                 {
                     test: /\.(png|jpe?g|gif)$/i,
-                    use: "file-loader",
+                    use: "file-loader"
                 },
                 {
                     test: /\.(ttf|otf|woff|woff2|eot)$/i,
-                    use: "file-loader",
+                    use: "file-loader"
                 },
                 {
                     test: /\.css$/i,
-                    use: ["style-loader", "css-loader"],
+                    use: ["style-loader", "css-loader"]
                 },
                 {
                     test: /\.s[ac]ss$/i,
-                    use: ["style-loader", "css-loader", "sass-loader"],
-                },
-            ],
+                    use: [
+                        "style-loader",
+                        "css-loader",
+                        {
+                            loader: "sass-loader",
+                            options: {
+                                implementation: require("sass")
+                            }
+                        }
+                    ]
+                }
+            ]
         },
 
         output: {
             filename: "index.js",
-            path: path.resolve(__dirname, "./build/renderer"),
+            path: path.resolve(__dirname, "./build/renderer")
         },
 
         plugins: [
             new HTMLWebpackPlugin({
                 filename: "index.html",
                 inject: "body",
-                template: path.resolve(__dirname, "./resources/renderer.pug"),
+                template: path.resolve(__dirname, "./resources/renderer.pug")
             }),
             new CopyWebpackPlugin({
                 patterns: [
                     {
                         from: BCUPUI_ICONS_PATH,
-                        to: "icons",
-                    },
-                ],
+                        to: "icons"
+                    }
+                ]
             }),
             new DefinePlugin({
                 __CORE_VERSION__: JSON.stringify(bcupCoreInfo.version),
-                __VERSION__: JSON.stringify(pkgInfo.version),
-            }),
+                __VERSION__: JSON.stringify(pkgInfo.version)
+            })
         ],
 
         resolve: {
             alias: {
-                buttercup: require.resolve("buttercup/web"),
+                buttercup: require.resolve("buttercup/web")
             },
-            extensions: [".tsx", ".ts", ".js"],
+            extensions: [".tsx", ".ts", ".js"]
         },
 
         target: "electron-renderer",
 
         watchOptions: {
             poll: 1000,
-            ignored: /node_modules/,
-        },
-    },
+            ignored: /node_modules/
+        }
+    }
 ];
