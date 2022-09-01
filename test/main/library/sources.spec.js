6119e0a3f5e102e84c1842f0ad3484f92006c56a
Perry Mitchell <perry@perrymitchell.net>
2022-09-01 20:35:47 +0300

    Fix tests

--- test/main/library/sources.spec.js
+++ test/main/library/sources.spec.js
@@ -1,30 +1,78 @@
-const { Credentials, VaultSource, VaultSourceStatus } = require("buttercup");
+const {
+    Credentials,
+    MemoryDatasource,
+    Vault,
+    VaultFormatA,
+    VaultFormatID,
+    VaultSource,
+    VaultSourceStatus,
+    setDefaultFormat
+} = require("buttercup");
 const { describeSource } = require("../../../build/main/library/sources.js");
+
+async function createTextSourceCredentials() {
+    const memoryProperty = `test${Math.random()}`;
+    const vault = new Vault();
+    const general = vault.createGroup("General");
+    general.createEntry("Login").setProperty("username", "user").setProperty("password", "test");
+    const mds = new MemoryDatasource(
+        Credentials.fromDatasource({ type: "memory", property: memoryProperty }, "test")
+    );
+    const encrypted = await mds.save(vault.format.getHistory(), Credentials.fromPassword("test"));
+    const creds = Credentials.fromDatasource(
+        {
+            type: "memory",
+            content: encrypted,
+            property: memoryProperty
+        },
+        "test"
+    );
+    return creds.toSecureString();
+}
 
 describe("library/sources", function () {
     describe("describeSource", function () {
         beforeEach(async function () {
-            const creds = Credentials.fromDatasource(
-                {
-                    type: "googledrive",
-                    fileID: "x123",
-                    token: "abc",
-                    refreshToken: "abc"
-                },
-                "test"
-            );
-            const credStr = await creds.toSecureString();
-            this.source = new VaultSource("Test Source", "googledrive", credStr);
+            setDefaultFormat(VaultFormatA);
+            const credStr = await createTextSourceCredentials();
+            this.source = new VaultSource("Test Source", "memory", credStr);
         });
 
-        it("returns the correct properties for a source", function () {
-            const description = describeSource(this.source);
-            expect(description).to.deep.equal({
-                id: this.source.id,
-                name: "Test Source",
-                state: VaultSourceStatus.Locked,
-                type: "googledrive",
-                order: 1000
+        afterEach(function () {
+            setDefaultFormat();
+        });
+
+        describe("when source locked", function () {
+            it("returns the correct properties for a source", function () {
+                const description = describeSource(this.source);
+                expect(description).to.deep.equal({
+                    id: this.source.id,
+                    name: "Test Source",
+                    state: VaultSourceStatus.Locked,
+                    type: "memory",
+                    order: 1000,
+                    format: null
+                });
+            });
+        });
+
+        describe("when source unlocked", function () {
+            beforeEach(async function () {
+                await this.source.unlock(Credentials.fromPassword("test"), {
+                    storeOfflineCopy: false
+                });
+            });
+
+            it("returns the correct properties for a source", function () {
+                const description = describeSource(this.source);
+                expect(description).to.deep.equal({
+                    id: this.source.id,
+                    name: "Test Source",
+                    state: VaultSourceStatus.Unlocked,
+                    type: "memory",
+                    order: 1000,
+                    format: VaultFormatID.A
+                });
             });
         });
     });
