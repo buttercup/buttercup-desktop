3c0682be9f045b3bf9cf169e0f873a88c3c4f15b
Perry Mitchell <perry@perrymitchell.net>
2023-02-23 21:46:12 +0200

    Setup entry searching for API

--- source/main/services/browser/controllers/entries.ts
+++ source/main/services/browser/controllers/entries.ts
@@ -1,12 +1,15 @@
 import { Request, Response } from "express";
 import { SearchResult } from "buttercup";
 import { EntriesSearchPayloadSchema, EntriesSearchType } from "../models";
+import { searchAllVaultsByTerm, searchAllVaultsByURL } from "../../search";
 
 export async function searchEntries(req: Request, res: Response) {
     const config = EntriesSearchPayloadSchema.parse(req.body);
     let results: Array<SearchResult> = [];
     if (config.type === EntriesSearchType.Term) {
+        results = await searchAllVaultsByTerm(config.term);
     } else if (config.type === EntriesSearchType.URL) {
+        results = await searchAllVaultsByURL(config.url);
     }
     res.json({
         results
