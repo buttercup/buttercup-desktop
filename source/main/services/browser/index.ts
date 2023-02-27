7cd39b0775626cf5b7d4d5e8e47af4bf43874930
Perry Mitchell <perry@perrymitchell.net>
2023-02-27 22:15:24 +0200

    Prepare API start

--- source/main/services/browser/index.ts
+++ source/main/services/browser/index.ts
@@ -1,3 +1,17 @@
-export async function start() {}
+import { IncomingMessage, Server, ServerResponse } from "node:http";
+import { Application } from "express";
+import { BROWSER_API_HOST_PORT } from "../../symbols";
+import { buildApplication } from "./api";
+
+let __app: Application | null = null,
+    __server: Server<typeof IncomingMessage, typeof ServerResponse>;
+
+export async function start(): Promise<void> {
+    if (__app) return;
+    __app = buildApplication();
+    return new Promise<void>((resolve) => {
+        __server = __app.listen(BROWSER_API_HOST_PORT, resolve);
+    });
+}
 
 export async function stop() {}
