cf6f0c1d2304cb82cbe98c4251d92d5c8301c15a
Quentin Harnay <quentin.harnay@arx.one>
2023-11-20 14:04:02 +0100

    Fix : try to change language

--- source/shared/i18n/trans.ts
+++ source/shared/i18n/trans.ts
@@ -1,9 +1,11 @@
 import i18next, { TOptions } from "i18next";
 import translations from "./translations/index";
 import { DEFAULT_LANGUAGE } from "../symbols";
+import changeLanguage from "@buttercup/ui";
 
 export async function changeLanguage(lang: string) {
     await i18next.changeLanguage(lang);
+    await changeLanguage(lang);
 }
 
 export async function initialise(lang: string) {
