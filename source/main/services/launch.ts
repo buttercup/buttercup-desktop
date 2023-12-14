85ac97149a353f7a5c9945f9eb0011caceab83ad
Perry Mitchell <perry@perrymitchell.net>
2023-12-14 08:30:14 +0200

    Fix auto launch for linux

--- source/main/services/launch.ts
+++ source/main/services/launch.ts
@@ -1,6 +1,44 @@
+import path from "node:path";
+import fs from "node:fs/promises";
 import { app } from "electron";
+import untildify from "untildify";
+import { pathExists } from "path-exists";
+import { isLinux } from "../../shared/library/platform";
+
+const LINUX_AUTOSTART_DIR = "~/.config/autostart";
+const LINUX_DESKTOP = `
+[Desktop Entry]
+Type=Application
+Version=1.0
+Name={{APP_NAME}}
+Comment={{APP_NAME}} startup script
+Exec={{APP_PATH}} --autostart
+StartupNotify=false
+Terminal=false
+`;
 
 export async function setStartWithSession(enable: boolean): Promise<void> {
+    if (isLinux()) {
+        await setStartWithSessionLinux(enable);
+    } else {
+        await setStartWithSessionNative(enable);
+    }
+}
+
+async function setStartWithSessionLinux(enable: boolean): Promise<void> {
+    const autostartPath = path.join(untildify(LINUX_AUTOSTART_DIR), "buttercup.desktop");
+    const isEnabled = await pathExists(autostartPath);
+    if (enable && !isEnabled) {
+        const desktop = LINUX_DESKTOP.trim()
+            .replace(/{{APP_NAME}}/g, "Buttercup")
+            .replace(/{{APP_PATH}}/g, process.execPath);
+        await fs.writeFile(autostartPath, desktop);
+    } else if (!enable && isEnabled) {
+        await fs.unlink(autostartPath);
+    }
+}
+
+async function setStartWithSessionNative(enable: boolean): Promise<void> {
     const isEnabled = app.getLoginItemSettings().openAtLogin;
     if (enable && !isEnabled) {
         app.setLoginItemSettings({
