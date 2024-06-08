2ae9746b95c0a9ca3bc535c686693c57b5314244
Perry Mitchell <perry@perrymitchell.net>
2024-06-08 22:12:04 +0300

    Merge UI types/symbols/tools

--- source/shared/library/clone.ts
+++ source/shared/library/clone.ts
@@ -1,7 +1,14 @@
 export function naiveClone<T extends { [key: string]: any }>(obj: T): T {
     const clone = { ...obj };
     for (const key in clone) {
-        if (typeof clone[key] === "object" && clone[key]) {
+        if (Array.isArray(clone[key])) {
+            clone[key] = clone[key].map((value) => {
+                if (typeof value === "object" && value) {
+                    return naiveClone(value);
+                }
+                return value;
+            });
+        } else if (typeof clone[key] === "object" && clone[key]) {
             clone[key] = naiveClone(clone[key]);
         }
     }
