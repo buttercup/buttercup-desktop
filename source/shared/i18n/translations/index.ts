96d9bcd608dfd3ea79feb303d53421910d2c4732
Peter Dave Hello <hsu@peterdavehello.org>
2024-07-01 00:59:50 +0800

    Add zh_tw Traditional Chinese translation

--- source/shared/i18n/translations/index.ts
+++ source/shared/i18n/translations/index.ts
@@ -14,6 +14,7 @@ import ro from "./ro.json";
 import ru from "./ru.json";
 import se from "./se.json";
 import zh_cn from "./zh_cn.json";
+import zh_tw from "./zh_tw.json";
 
 export default {
     en, // Keep as first item
@@ -32,5 +33,6 @@ export default {
     ro,
     ru,
     se,
-    zh_cn
+    zh_cn,
+    zh_tw
 };
